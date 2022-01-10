63b58b3d3844c43260ace90d595846d6e5bbaaf5
KDESP-LAPTOP\Konstantinos <kdesp2003@gmail.com>
2022-01-10 21:31:19 +0200

    functions

--- Encryption-Decryption/Encryption-Decryption.c
+++ Encryption-Decryption/Encryption-Decryption.c
@@ -1,9 +1,23 @@
 //Encryption - Decryption Project
-
 #include <stdio.h>
 #include <stdlib.h>
 
+//Programm
+
+//Menu
+
+//Verify
+
+//create key through hash
+
+//create hash
+
+//Decryption
+
+//Encryption
+
 int main(){
+	
 	
 	system("pause");
 	return 0;
