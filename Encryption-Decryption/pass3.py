8e094bf53b3b56089335a372977240d687d0473d
creatorkostas <iee2021106@iee.ihu.gr>
2022-01-17 20:39:22 +0200

    update

---
+++ Encryption-Decryption/pass3.py
@@ -0,0 +1,29 @@
+isdfkjsdflgl
+dfbsngmjdfxb
+fgfsnbrhgmn tqdm
+
+chasdjasdkvjlkefkglkafhalkgfm/ldsmkldsahfoioewjwem/;lf
+WEFKJAFJKLewkflSDJFDSfSDF
+sedfjDSNFJdsklfnkld():
+	sdkjflksdjf;ldskdf;sko;ks length: "))
+    length = 4#int(input("Enter sdffrd length: "))
+    
+    random.shuffle(characters)
+    password = []
+    for i in range(length):
+        password.appnd(random.choice(characters))
+
+    raER,LGLK;AFDKVLKFMBGLKDFVNVLF
+    FDGBDFBAFBEFDNH DFBFBRSNGV
+    CB ADFBSGNFHSDFCNDGHMSKIUKJSTukmdgh
+    dfjdfjndfdnjdtgn
+    return pasw
+  
+def str2hash(md5sring): # = "GeeksforGeeks"
+    result = hashlib.md5(md5sring.encode())
+    #print(result.hexdigest())
+    
+pasefklc;ladfkv/lkDSNVKLDF,VLBGKJBK;LREV
+GFDG
+ADFGAHDFAMGJFHJNBADFKMNG;LGSN
+DSFBKJADFNBLS;LKDMBADF
