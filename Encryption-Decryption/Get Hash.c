973e1afa8fc2e4a7df7c60a93bc545fe3c1b4292
KDESP-LAPTOP\Konstantinos <kdesp2003@gmail.com>
2022-01-20 14:35:39 +0200

    Hash corrections

--- Encryption-Decryption/Get Hash.c
+++ Encryption-Decryption/Get Hash.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_FILE_SIZE 300
+
 //WORKS
 void getHashFromFile(char file[], char hash[], int sizeofHash){
 	FILE *fp;
@@ -38,20 +40,20 @@ void getWholeFile(char file[], char buffer[], int maxSize){
 }
 
 //WORKS
-int wholeFileSize(char contents[]){
+int fileSize(char contents[]){
 	return strlen(contents);
 }
 
 //WORKS
 int realFileSize(char contents[], int sizeofHash, int sizeofGarbage){
-	return wholeFileSize(contents) - sizeofHash - sizeofGarbage;
+	return fileSize(contents) - sizeofHash - sizeofGarbage;
 }
 
 //WORKS
 void createRealFile(char newFileName[], char ogFileContents[], int sizeofHash, int sizeofGarbage, int sizeofFile, int sizeofRealFile){
 	FILE *nfp = fopen(newFileName, "w+");
 	
-	char newFileContents[200];
+	char newFileContents[MAX_FILE_SIZE];
 	
 	int i,j=0;
 	for(i = sizeofHash; i < sizeofFile - sizeofGarbage; i++){
@@ -59,6 +61,7 @@ void createRealFile(char newFileName[], char ogFileContents[], int sizeofHash, i
 		j++;
 	}
 	
+	printf("\n\nNew Contents: %s", newFileContents);
 	fputs(newFileContents, nfp); //Bazei mono to alhthino periexomeno se ena neo arxeio wste na ginei to Decryption
 	
 	fclose(nfp);
@@ -66,17 +69,17 @@ void createRealFile(char newFileName[], char ogFileContents[], int sizeofHash, i
 
 int main(){
 	char hash[64];
-	char contents[200];
+	char contents[MAX_FILE_SIZE];
 	
-	getHashFromFile("file.txt", hash, 64);
+	getHashFromFile("ogencryptedfile.txt", hash, 64);
 	printf("Hash: %s", hash);
 	
-	getWholeFile("file.txt", contents, 200);
+	getWholeFile("ogencryptedfile.txt", contents, MAX_FILE_SIZE);
 	printf("\n\nContents: %s", contents);
 	
-	createRealFile("newFile.txt", contents, 64, 10, wholeFileSize(contents), realFileSize(contents, 64, 10));
+	createRealFile("encryptedFile.txt", contents, 64, 10, fileSize(contents), realFileSize(contents, 64, 10));
 	
-		
+	
 	printf("\n\n");
 	system("pause");
 	return 0;
